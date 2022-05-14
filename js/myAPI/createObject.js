9f4c04b41531b38ca821df66b2689391f5a74581
Duanyuqi397 <641045913@qq.com>
2022-05-14 19:18:07 +0800

    通过context存储全局用户信息，实现JWT，实现自定义useAuth，实现登陆状态与非登陆状态切换

--- js/myAPI/createObject.js
+++
@@ -1,9 +0,0 @@
-//object.create
-
-function create(proto){
-    function F(){};
-    F.prototype = proto;
-    F.prototype.constructor = F;
-
-    return new F();
-}
---
+++ notes/项目阶段/index.md
@@ -0,0 +1,3 @@
+地址：https://github.com/Duanyuqi397/my-jira/tree/remake-duan
+
+通过context存储全局用户信息，实现JWT，实现自定义useAuth，实现登陆状态与非登陆状态切换
