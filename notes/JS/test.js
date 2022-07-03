5e1851f41c9b73e8eeabf921630b798494e2412e
Duanyuqi397 <641045913@qq.com>
2022-07-03 18:52:10 +0800

    chore: update

--- notes/JS/test.js
+++ notes/JS/test.js
@@ -1,53 +1,16 @@
-// function myDebounce(fn,delay){
-//     let timer = null;
-//     return function(){
-//         clearTimeout(timer);
-//         timer = setTimeout(() => {
-//             fn()
-//         },delay);
-//     }
-// }
-
-function A(config) {
-    config.age = 20
-    return config
-  }
-  function B(config) {
-    config.city = '上海'
-    return config
-  }
-  function C(config) {
-    config.company = '再惠'
-    return config
-  }
-
-function compose(...funcs){
-    let len = funcs.length;
-    let result;
-    let count = len - 1;
-    return  function fn(...args){
-        result = funcs[count].apply(this,args);
-        if(count < 0){
-            return result;
-        }
-        count--;
-        return fn(result);
+function test(dirarr,docidarr){
+  let filearr = [{docid:1},{docid:2},{docid:3},{docid:1}];
+  return new Promise((resolve,reject) => {
+    const uploadarr = [...new Set(filearr)].concat(...new Set(dirarr));
+    // console.log(uploadarr)
+    if(!docidarr.includes(uploadarr[0]?.docid)){
+      console.log('come in')
+      uploadarr[0]?.docid && docidarr.push(uploadarr[0].docid);
+      resolve(uploadarr);
     }
+  })
 }
 
-console.log(compose(A, B, C)({ name: 'demo' }))
-
-// function fb(n){
-//     if(n <= 0){
-//         return 0;
-//     }
-//     if(n == 1 ){
-//         return 1;
-//     }
-//     // if(n == 2){
-//     //     return 3;
-//     // }
-//     return fb(n - 1) + fb(n - 2);
-// }
-
-// console.log(fb(10))
+const res1 = test([{docid:99}],[1]);
+const res2 = test([{docid:99}],[6]);
+console.log(res1,res2);
